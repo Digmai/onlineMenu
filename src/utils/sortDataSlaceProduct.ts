91ccd857f80bbb0a468c72ad711cd17e3fb66c6c
Digmai <qt20130@gmail.com>
2023-05-12 16:04:10 +0300

    rewrote product grouping function

--- src/utils/sortDataSlaceProduct.ts
+++ src/utils/sortDataSlaceProduct.ts
@@ -1,22 +1,15 @@
 import { IProduct } from "../types";
 
 export function sortDataSlaceProduct(data: IProduct[]) {
-  const sortedData: {
+  return data.reduce<{
     [category: string]: { [subcategory: string]: IProduct[] };
-  } = {};
-  data.forEach((item) => {
+  }>((sortedData, item) => {
     const { category, subcategory } = item;
-    if (!sortedData[category]) {
-      sortedData[category] = {};
-    }
-    if (subcategory && !sortedData[category][subcategory]) {
-      sortedData[category][subcategory] = [];
-    }
-    if (subcategory) {
-      sortedData[category][subcategory].push(item);
-    } else {
-      sortedData[category][""] = [item];
-    }
-  });
-  return sortedData;
+    sortedData[category] = sortedData[category] || {};
+    sortedData[category][subcategory || ""] = [
+      ...(sortedData[category][subcategory || ""] || []),
+      item,
+    ];
+    return sortedData;
+  }, {});
 }
